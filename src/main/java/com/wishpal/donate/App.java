cefc899968f8ec8ca0486da03d58fc11de748bb0
Shufeng01 <fenghhk@gmail.com>
2014-10-11 18:56:48 -0700

    add files

--- src/main/java/com/wishpal/donate/App.java
+++ src/main/java/com/wishpal/donate/App.java
@@ -32,55 +32,114 @@ import com.google.zxing.common.BitMatrix;
 import com.google.zxing.common.HybridBinarizer;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
-
 public class App {
-    public static void main(String[] args) {
-        System.setProperty(SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "DEBUG");
+	public static final String URL = "http://127.0.0.1:4567";
 
-        staticFileLocation("/");
+	public static void main(String[] args) {
+		System.setProperty(SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "INFO");
 
-        //Heroku will pass $PORT to the app. The default port is 4567
-        String portStr = System.getenv("PORT");
-        if(portStr != null) setPort(Integer.parseInt(portStr));
+		staticFileLocation("/");
 
-    	
-        get(new Route("/") {
-        	@Override
-        	public Object handle(Request req, Response resp) {
-        		return "hello nihao";
-        	}
-        });
-        
-        get(new FreeMarkerRoute("/") {
-            @Override
-            public Object handle(Request request, Response response) {
-                Map<String, Object> viewObjects = new HashMap<String, Object>();
+		// Heroku will pass $PORT to the app. The default port is 4567
+		String portStr = System.getenv("PORT");
+		if (portStr != null)
+			setPort(Integer.parseInt(portStr));
 
-                viewObjects.put("templateName", "articleForm.ftl");
+		get(new Route("/hello") {
+			@Override
+			public Object handle(Request req, Response resp) {
+				return "hello nihao";
+			}
+		});
 
-                return modelAndView(viewObjects, "index.ftl");
-            }
-        });
+		get(new FreeMarkerRoute("/") {
+			@Override
+			public Object handle(Request request, Response response) {
+				Map<String, Object> viewObjects = new HashMap<String, Object>();
+				viewObjects.put("templateName", "wishItem.ftl");
+				viewObjects.put("imgPath", "");
 
-    }
-    
-	public static void createQRCode(String qrCodeData, String filePath,
-			String charset, Map hintMap, int qrCodeheight, int qrCodewidth)
-			throws WriterException, IOException {
-		BitMatrix matrix = new MultiFormatWriter().encode(
-				new String(qrCodeData.getBytes(charset), charset),
-				BarcodeFormat.QR_CODE, qrCodewidth, qrCodeheight, hintMap);
-		MatrixToImageWriter.writeToFile(matrix, filePath.substring(filePath
-				.lastIndexOf('.') + 1), new File(filePath));
+				return modelAndView(viewObjects, "index.ftl");
+			}
+		});
+
+		post(new FreeMarkerRoute("/submit") {
+			@Override
+			public Object handle(Request req, Response resp) {
+				String name = req.queryParams("name");
+				String codeData = URL + "/update/" + name;
+				String imgPath = "src/main/resources/image/" + name + ".png";
+				
+				try {
+					System.out.println(new java.io.File(".").getCanonicalPath());
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				createQRCode(codeData, imgPath);
+				
+				Map<String, Object> viewObjects = new HashMap<String, Object>();
+				viewObjects.put("templateName", "wishItem.ftl");
+				viewObjects.put("imgPath", "image/" + name + ".png");
+
+				return modelAndView(viewObjects, "index.ftl");
+			}
+		});
+
+		get(new FreeMarkerRoute("/update/:name") {
+			@Override
+			public Object handle(Request req, Response resp) {
+				String userName = req.params(":name");
+				if (userName == null) userName = "Amigo";
+				Map<String, Object> viewObjects = new HashMap<String, Object>();
+				viewObjects.put("templateName", "codeResult.ftl");
+				viewObjects.put("userName", userName);
+
+				return modelAndView(viewObjects, "index.ftl");
+			}
+		});
 	}
 
-	public static String readQRCode(String filePath, String charset, Map hintMap)
-			throws FileNotFoundException, IOException, NotFoundException {
-		BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(
-				new BufferedImageLuminanceSource(
-						ImageIO.read(new FileInputStream(filePath)))));
-		Result qrCodeResult = new MultiFormatReader().decode(binaryBitmap,
-				hintMap);
-		return qrCodeResult.getText();
+	public static void createQRCode(String qrCodeData, String filePath) {
+
+		String charset = "UTF-8"; // or "ISO-8859-1"
+		Map hintMap = new HashMap();
+		hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+		int qrCodeheight = 240;
+		int qrCodewidth = 240;
+
+		try {
+			BitMatrix matrix = new MultiFormatWriter().encode(new String(
+					qrCodeData.getBytes(charset), charset),
+					BarcodeFormat.QR_CODE, qrCodewidth, qrCodeheight, hintMap);
+
+			MatrixToImageWriter.writeToFile(matrix,
+					filePath.substring(filePath.lastIndexOf('.') + 1),
+					new File(filePath));
+			System.out.println("create qrcode in" + filePath);
+		} catch (WriterException ex) {
+			ex.printStackTrace();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public static String readQRCode(String filePath) {
+		String charset = "UTF-8"; // or "ISO-8859-1"
+		Map hintMap = new HashMap();
+		hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+		
+		try {
+			BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(
+					new BufferedImageLuminanceSource(
+							ImageIO.read(new FileInputStream(filePath)))));
+			Result qrCodeResult = new MultiFormatReader().decode(binaryBitmap, hintMap);
+			return qrCodeResult.getText();
+		}  catch (IOException ex) {
+			ex.printStackTrace();
+		} catch (NotFoundException ex) {
+			ex.printStackTrace();
+		}
+		return "Error";
 	}
 }
